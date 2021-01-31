df6ec7c0b388494b5455bb10708154c64ad53214
Lijun <gan.lijun.glj@gmail.com>
2021-01-30 19:53:21 -0500

    change files

--- skeleton/movie_buff/03_queries.rb
+++ skeleton/movie_buff/03_queries.rb
@@ -2,10 +2,22 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  Movie.select(:title,:id).joins(:actors)
+  .where( actors: {name: those_actors})
+  .group(:id)
+  .having('count(*) >= ?', those_actors.length)
+
+
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+  Movie
+  .select('AVG(score), (yr / 10) * 10 AS decade')
+  .group('decade')
+  .order('avg(score) DESC')
+  .first
+  .decade
 
 end
 
@@ -13,12 +25,13 @@ def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
-
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor.select(:name)
+  .left_outer_joins(:castings).where(castings: {movie_id: nil})
+  .count
 end
 
 def starring(whazzername)
@@ -36,5 +49,11 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
+  Actor
+  .select(:name, :id, 'MAX(movies.yr) - MIN(movies.yr) AS career')
+  .joins(:movies)
+  .order('career DESC, name')
+  .group(:id)
+  .limit(3)
 
 end
